b9297dcad04f3c2407addc7f66d230aba905df8d
Psychee <781513172@qq.com>
2017-04-27 15:58:56 +0800

    增加弹层

--- 网上商城/js/product.js
+++ 网上商城/js/product.js
@@ -14,3 +14,46 @@ var oTabBox = document.getElementById('right-bottom'),
 			aDiv[this.index].style.display = 'block';
 		}
 	}
+//弹层
+var oMainLeft = document.getElementById('main-left');
+var aDialogLi = oMainLeft.getElementsByTagName('li');
+var oDialogBox = getClass('dialog-box')[0];
+var oDialogBody = getClass('dialog-body')[0];
+var oDialogImg = oDialogBody.getElementsByTagName('img')[0];
+var oDialogClose = getClass('dialog-close')[0];
+var oDialogPrev = getClass('dialog-prev',oDialogBox)[0];
+var oDialogNext = getClass('dialog-next',oDialogBox)[0];
+var iNow = 0;
+
+for (var i = 0; i < aDialogLi.length; i++) {
+	aDialogLi[i].index = i;
+	aDialogLi[i].onclick = function(){
+		var oImg = this.getElementsByTagName('img')[0];
+		oDialogBox.style.display = 'block';
+		oDialogImg.src = oImg.src;
+		iNow = this.index;
+	}
+}
+//关闭弹层
+oDialogBox.onclick = function(e){
+	var target = e.target || event.srcElement;
+	if(target == oDialogBox || target == oDialogClose){
+		oDialogBox.style.display = 'none';
+	}
+}
+oDialogPrev.onclick = function(){
+	iNow--;
+	if(iNow == -1){
+		iNow = aDialogLi.length -1;
+	}
+	var prevLi = aDialogLi[iNow];
+	oDialogImg.src = prevLi.getElementsByTagName('img')[0].src;
+}
+oDialogNext.onclick = function(){
+	iNow++;
+	if(iNow == aDialogLi.length){
+		iNow = 0;
+	}
+	var prevLi = aDialogLi[iNow];
+	oDialogImg.src = prevLi.getElementsByTagName('img')[0].src;
+}
